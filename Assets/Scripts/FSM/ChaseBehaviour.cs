037bbc705bf111ba06b48e45bda3891d80620b6e
Nil-Puente <npuentel@edu.tecnocampus.cat>
2026-02-05 16:09:19 +0100

    Added Template Scripts
    I Added Template Scripts

---
+++ Assets/Scripts/FSM/ChaseBehaviour.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ChaseBehaviour : StateMachineBehaviour
+{
+    public float Speed = 2;
+    public float VisionRange;
+
+    private Transform player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var playerClose = IsPlayerClose(animator.transform);
+        animator.SetBool("IsChasing", playerClose);
+        Vector2 dir = player.position - animator.transform.position;
+        animator.transform.position += (Vector3)dir.normalized * Speed * Time.deltaTime;
+    }
+
+    private bool IsPlayerClose(Transform transform)
+    {
+        var dist = Vector3.Distance(transform.position, player.position);
+        return (dist < VisionRange);
+    }
+}
