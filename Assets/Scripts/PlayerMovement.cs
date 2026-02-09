7c104ff8c7a19feb1540344cc1e80e3fc0ca93f8
Gisela Vidal <vidal.gisela.g@gmail.com>
2026-02-09 17:10:44 +0100

    Error Rotation de Assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -33,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
         {
             float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, 0, angle);
+            
         }
     }
 
