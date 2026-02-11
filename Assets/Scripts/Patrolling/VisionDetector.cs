4b4cd20ad1fdbe7e2ebccb41c5ceb0587a2afbdd
66aran66 <apuentel@edu.tecnocampus.cat>
2026-02-11 20:05:56 +0100

    Update Enemy Cone+Kill

--- Assets/Scripts/Patrolling/VisionDetector.cs
+++ Assets/Scripts/Patrolling/VisionDetector.cs
@@ -12,7 +12,7 @@ public class VisionDetector : MonoBehaviour
     public float DetectionRange = 3f;
     public float VisionAngle = 90f;
 
-    public float RestartDistance = 0.5f;
+    [SerializeField] private float restartDistance;
 
     private LineRenderer lr;
     public int segments = 20;
@@ -21,22 +21,33 @@ public class VisionDetector : MonoBehaviour
     {
         lr = GetComponent<LineRenderer>();
         if (lr == null)
+        {
             lr = gameObject.AddComponent<LineRenderer>();
 
-        lr.positionCount = segments + 3;
+            lr.positionCount = segments + 3;
 
-        lr.useWorldSpace = true;
-        lr.startWidth = 0.03f;
-        lr.endWidth = 0.03f;
-        lr.material = new Material(Shader.Find("Sprites/Default"));
-        lr.startColor = new Color(1f, 1f, 0f, 0.4f);
-        lr.endColor = new Color(1f, 1f, 0f, 0.4f);
+            lr.useWorldSpace = true;
+            lr.startWidth = 0.03f;
+            lr.endWidth = 0.03f;
+            lr.material = new Material(Shader.Find("Sprites/Default"));
+            lr.startColor = new Color(1f, 1f, 0f, 0.4f);
+            lr.endColor = new Color(1f, 1f, 0f, 0.4f);
+        }
     }
 
     private void Update()
     {
         bool playerDetected = DetectPlayers().Length > 0;
         GetComponent<Animator>().SetBool("IsChasing", playerDetected);
+
+        if (playerDetected)
+        {
+            SetConeColor(new Color(1f,0f,0f,0.4f));
+        }
+        else
+        {
+            SetConeColor(new Color(1f, 1f, 0f, 0.4f));
+        }
 
         DrawVisionCone();
     }
@@ -82,12 +93,12 @@ public class VisionDetector : MonoBehaviour
 
     private bool PlayerInRange(ref List<Transform> players)
     {
-        Collider2D[] playerColliders =
-            Physics2D.OverlapCircleAll(transform.position, DetectionRange, WhatIsPlayer);
+        Collider2D[] playerColliders = Physics2D.OverlapCircleAll(transform.position, DetectionRange, WhatIsPlayer);
 
         foreach (var item in playerColliders)
+        {
             players.Add(item.transform);
-
+        }
         return players.Count > 0;
     }
 
@@ -101,7 +112,9 @@ public class VisionDetector : MonoBehaviour
             float angle = Vector2.Angle(forward, targetDir);
 
             if (angle > VisionAngle / 2f)
+            {
                 players.RemoveAt(i);
+            }
         }
 
         return players.Count > 0;
@@ -112,7 +125,9 @@ public class VisionDetector : MonoBehaviour
         for (int i = players.Count - 1; i >= 0; i--)
         {
             if (!IsVisible(players[i]))
+            {
                 players.RemoveAt(i);
+            }
         }
 
         return players.Count > 0;
@@ -141,10 +156,15 @@ public class VisionDetector : MonoBehaviour
         {
             float distance = Vector2.Distance(transform.position, player.position);
 
-            if (distance <= RestartDistance)
+            if (distance <= restartDistance)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
+    }
+    private void SetConeColor(Color c)
+    {
+        lr.startColor = c;
+        lr.endColor = c;
     }
 }
